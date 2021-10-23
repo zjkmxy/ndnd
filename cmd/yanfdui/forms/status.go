05cb55ac1b8db8d5b50de86f8f3d139f411e0a8e
Varun Patil <radialapps@gmail.com>
2021-10-23 15:07:02 -0700

    Add face list to status

--- cmd/yanfdui/forms/status.go
+++ cmd/yanfdui/forms/status.go
@@ -9,33 +9,44 @@ package forms
 
 import (
 	"fmt"
+	"sort"
 	"time"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 	"github.com/named-data/YaNFD/core"
+	"github.com/named-data/YaNFD/face"
 	"github.com/named-data/YaNFD/table"
 )
 
 type StatusForm struct {
 	refresh chan uint
 	ticker  *time.Ticker
-	table   *widgets.Table
+	general *widgets.Table
+	faces   *widgets.Table
 }
 
 func NewStatusForm() *StatusForm {
 	ticker := time.NewTicker(1 * time.Second)
 	refresh := make(chan uint)
-	tab := widgets.NewTable()
 
-	tab.Rows = [][]string{
+	general := widgets.NewTable()
+	general.Title = "General"
+	general.Rows = [][]string{
 		{"version", ""},
 		{"startTime", ""},
 		{"currentTime", ""},
-		{"upTime", ""},
+		{"nFibEntries", ""},
 	}
-	tab.TextStyle = ui.NewStyle(ui.ColorWhite)
-	tab.SetRect(0, 5, 70, 20)
+	general.TextStyle = ui.NewStyle(ui.ColorWhite)
+	general.SetRect(0, 5, 70, 14)
+
+	faces := widgets.NewTable()
+	faces.Title = "Faces"
+	faces.Rows = [][]string{}
+	faces.TextAlignment = ui.AlignCenter
+	faces.TextStyle = ui.NewStyle(ui.ColorWhite)
+	faces.SetRect(0, 15, 70, 30)
 
 	go func() {
 		for {
@@ -46,7 +57,8 @@ func NewStatusForm() *StatusForm {
 	return &StatusForm{
 		refresh: refresh,
 		ticker:  ticker,
-		table:   tab,
+		general: general,
+		faces:   faces,
 	}
 }
 
@@ -58,10 +70,28 @@ func (f *StatusForm) Render() {
 	// Don't set NNameTreeEntries because we don't use a NameTree
 	nFibEntries := uint64(len(table.FibStrategyTable.GetAllFIBEntries()))
 
-	f.table.Rows[0][1] = core.Version
-	f.table.Rows[1][1] = core.StartTimestamp.Local().String()
-	f.table.Rows[2][1] = time.Now().Local().String()
-	f.table.Rows[3][1] = fmt.Sprint(nFibEntries)
+	// General status table
+	f.general.Rows[0][1] = core.Version
+	f.general.Rows[1][1] = core.StartTimestamp.Local().String()
+	f.general.Rows[2][1] = time.Now().Local().String()
+	f.general.Rows[3][1] = fmt.Sprint(nFibEntries)
 
-	ui.Render(f.table)
+	// Face list table
+	f.faces.Rows = [][]string{}
+	for _, face := range face.FaceTable.GetAll() {
+		f.faces.Rows = append(f.faces.Rows,
+			[]string{fmt.Sprint(face.FaceID()), face.LocalURI().String()},
+		)
+	}
+
+	// Sort by face ID
+	sort.Slice(f.faces.Rows, func(i, j int) bool {
+		return f.faces.Rows[i][0] < f.faces.Rows[j][0]
+	})
+
+	// Add header row
+	f.faces.Rows = append([][]string{{"ID", "Local URI"}}, f.faces.Rows...)
+
+	ui.Render(f.general)
+	ui.Render(f.faces)
 }
