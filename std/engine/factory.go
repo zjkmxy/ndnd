cf2410610994269985de3a8b231463d2076577cb
Varun Patil <varunpatil@ucla.edu>
2025-01-02 19:02:33 +0000

    std: implement client configuration

--- std/engine/factory.go
+++ std/engine/factory.go
@@ -1,6 +1,10 @@
 package engine
 
 import (
+	"fmt"
+	"net/url"
+	"os"
+
 	enc "github.com/named-data/ndnd/std/encoding"
 	"github.com/named-data/ndnd/std/engine/basic"
 	"github.com/named-data/ndnd/std/engine/face"
@@ -23,5 +27,25 @@ func NewUnixFace(addr string) face.Face {
 }
 
 func NewDefaultFace() face.Face {
-	return NewUnixFace("/var/run/nfd/nfd.sock")
+	config := GetClientConfig()
+
+	// Parse transport URI
+	uri, err := url.Parse(config.TransportUri)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse transport URI %s: %v (invalid client config)\n", uri, err)
+		os.Exit(1)
+	}
+
+	if uri.Scheme == "unix" {
+		return NewUnixFace(uri.Path)
+	}
+
+	if uri.Scheme == "tcp" || uri.Scheme == "tcp4" || uri.Scheme == "tcp6" {
+		return face.NewStreamFace(uri.Scheme, uri.Host, false)
+	}
+
+	fmt.Fprintf(os.Stderr, "Unsupported transport URI: %s (invalid client config)\n", uri)
+	os.Exit(1)
+
+	return nil
 }
