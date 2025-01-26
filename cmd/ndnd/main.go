a96d91a5bfc10e372903c87a108c126cc0698dcb
Varun Patil <varunpatil@ucla.edu>
2025-01-26 04:24:48 +0000

    ndnd: version printing

--- cmd/ndnd/main.go
+++ cmd/ndnd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/named-data/ndnd/cmd"
 	dv "github.com/named-data/ndnd/dv/executor"
 	fw "github.com/named-data/ndnd/fw/executor"
+	"github.com/named-data/ndnd/std/utils"
 	"github.com/named-data/ndnd/std/utils/toolutils"
 	"github.com/named-data/ndnd/tools"
 	"github.com/named-data/ndnd/tools/dvc"
@@ -16,7 +18,7 @@ import (
 func main() {
 	tree := toolutils.CmdTree{
 		Name: "ndnd",
-		Help: "Named Data Networking Daemon",
+		Help: fmt.Sprintf("Named Data Networking Daemon (%s)", utils.NDNdVersion),
 		Sub: []*toolutils.CmdTree{{
 			Name: "fw",
 			Help: "NDN Forwarding Daemon",
@@ -67,5 +69,12 @@ func main() {
 	// Parse the command line arguments
 	args := os.Args
 	args[0] = tree.Name
+
+	// Handle top level flags
+	if len(args) >= 2 && args[1] == "-version" {
+		fmt.Println(utils.NDNdVersion)
+		os.Exit(0)
+	}
+
 	tree.Execute(args)
 }
