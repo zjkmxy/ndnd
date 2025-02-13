a5bbab0089410601068a5b33b80d57190a96e995
Varun Patil <radialapps@gmail.com>
2025-02-13 08:24:35 -0800

    std: fix downstream tinygo build

--- fw/face/web-socket-transport.go
+++ fw/face/web-socket-transport.go
@@ -1,9 +1,4 @@
-/* YaNFD - Yet another NDN Forwarding Daemon
- *
- * Copyright (C) 2020-2021 Eric Newberry.
- *
- * This file is licensed under the terms of the MIT License, as found in LICENSE.md.
- */
+//go:build !tinygo
 
 package face
 
