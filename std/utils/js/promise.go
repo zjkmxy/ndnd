db83b0a1adede73c7bc140579d58ed43c389192c
Varun Patil <radialapps@gmail.com>
2025-02-23 12:58:53 -0800

    std: fix memleaking js utils

--- std/utils/js/promise.go
+++ std/utils/js/promise.go
@@ -7,8 +7,10 @@ import (
 	"syscall/js"
 )
 
+var promiseGlobal = js.Global().Get("Promise")
+
 func AsyncFunc(f func(this js.Value, p []js.Value) (any, error)) js.Func {
-	return js.FuncOf(func(this js.Value, p []js.Value) interface{} {
+	return js.FuncOf(func(this js.Value, p []js.Value) any {
 		promise, resolve, reject := Promise()
 		go func() {
 			ret, err := f(this, p)
@@ -24,31 +26,47 @@ func AsyncFunc(f func(this js.Value, p []js.Value) (any, error)) js.Func {
 
 func Promise() (promise js.Value, resolve func(args ...any), reject func(args ...any)) {
 	var jsResolve, jsReject js.Value
-	return js.Global().Get("Promise").New(js.FuncOf(func(this js.Value, args []js.Value) any {
-			jsResolve = args[0]
-			jsReject = args[1]
-			return nil
-		})), func(args ...any) {
-			jsResolve.Invoke(args...)
-		}, func(args ...any) {
-			jsReject.Invoke(args...)
-		}
+
+	promiseConstructor := js.FuncOf(func(this js.Value, args []js.Value) any {
+		jsResolve = args[0]
+		jsReject = args[1]
+		return nil
+	})
+
+	promise = promiseGlobal.New(promiseConstructor)
+	resolve = func(args ...any) { jsResolve.Invoke(args...) }
+	reject = func(args ...any) { jsReject.Invoke(args...) }
+
+	promiseConstructor.Release()
+	return
 }
 
 func Await(promise js.Value) (val js.Value, err error) {
-	resc := make(chan js.Value, 1)
-	errc := make(chan error, 1)
-	promise.Call("then", js.FuncOf(func(this js.Value, p []js.Value) interface{} {
-		resc <- p[0]
+	res := make(chan any, 1)
+
+	var resolve, reject js.Func
+	resolve = js.FuncOf(func(this js.Value, p []js.Value) any {
+		res <- p[0]
+		resolve.Release()
+		reject.Release()
 		return nil
-	})).Call("catch", js.FuncOf(func(this js.Value, p []js.Value) interface{} {
-		errc <- errors.New(p[0].String())
+	})
+	reject = js.FuncOf(func(this js.Value, p []js.Value) any {
+		res <- errors.New(p[0].String())
+		resolve.Release()
+		reject.Release()
 		return nil
-	}))
-	select {
-	case r := <-resc:
-		return r, nil
-	case e := <-errc:
-		return js.Undefined(), e
+	})
+
+	promise.Call("then", resolve).Call("catch", reject)
+
+	result := <-res
+	switch v := result.(type) {
+	case js.Value:
+		return v, nil
+	case error:
+		return js.Undefined(), v
+	default:
+		return js.Undefined(), errors.New("unexpected type")
 	}
 }
